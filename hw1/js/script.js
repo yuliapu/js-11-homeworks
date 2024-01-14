2784f8746386cbf96976fe17d6c8f6115687eae6
Yuliia Putova MTANA <yuliia.putova@mt.com>
2024-01-14 11:25:33 +0100

    hw 1

---
+++ hw1/js/script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+let firstNumber = 40;
+let secondNumber = 5;
+
+let additionResult = firstNumber + secondNumber;
+let subtractionResult = firstNumber - secondNumber;
+let multiplicationResult = firstNumber * secondNumber;
+let divisionResult = firstNumber / secondNumber;
+let exponentiationResult = firstNumber ** secondNumber;
+
+console.log(`Addition: ${firstNumber} + ${secondNumber} = ${additionResult}`);
+console.log(`Subtraction: ${firstNumber} - ${secondNumber} = ${subtractionResult}`);
+console.log(`Multiplication: ${firstNumber} * ${secondNumber} = ${multiplicationResult}`);
+console.log(`Division: ${firstNumber} / ${secondNumber} = ${divisionResult}`);
+console.log(`Exponentiation: ${firstNumber} ** ${secondNumber} = ${exponentiationResult}`);
