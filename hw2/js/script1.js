ef0f4d37d673bf77e99a7f9424ed9da0d5c18949
Yuliia Putova MTANA <yuliia.putova@mt.com>
2024-01-24 13:48:23 +0100

    hw2 - comments

--- hw2/js/script1.js
+++ hw2/js/script1.js
@@ -6,6 +6,6 @@ let volatileNumber = 78.39271;
 
 console.log("volatileNumber:", volatileNumber);
 
-console.log("String: ", volatileNumber + "");
+console.log("String: ", volatileNumber.toString());
 console.log("Number: ", +volatileNumber);
 console.log("Boolean: ", !!volatileNumber);
