fb42a4b9ec1ecd24e88785879e14315bc9faf280
Yuliia Putova MTANA <yuliia.putova@mt.com>
2024-03-17 15:49:02 +0100

    comments

--- hw13/js/script.js
+++ hw13/js/script.js
@@ -1,54 +1,70 @@
 "use strict";
 
 let switchButton = document.querySelector('button');
-let html = document.querySelector('html');
+let body = document.querySelector('body');
 let lastUpdatedTime = document.querySelector('p');
 
 const turnOffText = "Turn off";
 const turnOnText = "Turn on";
 const onStateColor = "LightYellow";
 const offStateColor = "MidnightBlue";
+const themeKey = "theme";
+const lastUpdateKey = "lastUpdate";
 
-let lastUpdatedStoredText = localStorage.getItem("lastUpdateText");
-if (lastUpdatedStoredText !== null){
-    lastUpdatedTime.textContent = lastUpdatedStoredText;
-    if (lastUpdatedStoredText.includes(turnOffText.toLowerCase())){
-        setTurnedOffStyles();
+let lastUpdatedStoredDateTime = localStorage.getItem(lastUpdateKey);
+let theme = localStorage.getItem(themeKey);
+
+if (theme !== null){
+    if (theme == turnOffText){
+        setTurnOffStylesAndText(lastUpdatedStoredDateTime);
     }
     else{
-        setTurnedOnStyles();
+        setTurnOnStylesAndText(lastUpdatedStoredDateTime);
     }
 }
 
-switchButton.addEventListener("click", () =>{
+switchButton.addEventListener("click", () => {
+    let lastUpdatedDateTime = new Date(Date.now());
+    localStorage.setItem(lastUpdateKey, lastUpdatedDateTime.toString());
     if(switchButton.textContent === turnOffText){
-        setTurnedOffStyles();
-        localStorage.setItem("lastUpdateText", getLastUpdateTime(Date.now(), turnOffText));
-        lastUpdatedTime.textContent = localStorage.getItem("lastUpdateText");
+        setTurnOffStylesAndText(lastUpdatedDateTime);
     }
     else {
-        setTurnedOnStyles();
-        localStorage.setItem("lastUpdateText", getLastUpdateTime(Date.now(), turnOnText));
-        lastUpdatedTime.textContent = localStorage.getItem("lastUpdateText");
+        setTurnOnStylesAndText(lastUpdatedDateTime);
     }
 });
 
 function setTurnedOnStyles(){
-    html.style.backgroundColor = onStateColor;
+    body.style.backgroundColor = onStateColor;
     lastUpdatedTime.style.color = offStateColor;
     switchButton.textContent = turnOffText;
 }
 
 function setTurnedOffStyles(){
-    html.style.backgroundColor = offStateColor;
+    body.style.backgroundColor = offStateColor;
     lastUpdatedTime.style.color = onStateColor;
     switchButton.textContent = turnOnText;
 }
 
 function getLastUpdateTime(datetimeInput, state){
+    const format = (input) => `${('0' + input).slice(-2)}`;
     let datetime = new Date(datetimeInput);
-    let formattedDateTime = `${datetime.getDate()}-${datetime.getMonth()}-${datetime.getFullYear()} 
-    ${datetime.getHours()}:${datetime.getMinutes()}:${datetime.getSeconds()}`;
+    let formattedDateTime = `${format(datetime.getDate())}-`+
+                             `${format(datetime.getMonth())}-`+
+                             `${datetime.getFullYear()} `+
+                             `${format(datetime.getHours())}:${format(datetime.getMinutes())}:${format(datetime.getSeconds())}`;
     
-    return`Last ${state.toString().toLowerCase()}: ${formattedDateTime}`;
+    return formattedDateTime;
+}
+
+function setTurnOnStylesAndText(datetimeInput){
+    localStorage.setItem(themeKey, turnOnText);
+    setTurnedOnStyles();
+    lastUpdatedTime.textContent = `Last ${turnOnText.toLowerCase()}: ${getLastUpdateTime(datetimeInput, turnOnText)}`;
+}
+
+function setTurnOffStylesAndText(datetimeInput){
+    localStorage.setItem(themeKey, turnOffText);
+    setTurnedOffStyles();
+    lastUpdatedTime.textContent = `Last ${turnOffText.toLowerCase()}: ${getLastUpdateTime(datetimeInput, turnOnText)}`;
 }
